cf7b654db9398a10f709d94dc19246890774e40e
liuruchen <3938352235@qq.com>
2026-09-20 22:11:53 +0800

    Update sidebarConfig.ts

--- src/config/sidebarConfig.ts
+++ src/config/sidebarConfig.ts
@@ -65,6 +65,16 @@ export const sidebarLayoutConfig: SidebarLayoutConfig = {
 			showOnPostPage: true,
 		},
 		{
+			// 组件类型：天气
+			type: "weather",
+			// 是否启用该组件
+			enable: true,
+			// 组件位置
+			position: "top",
+			// 是否在文章详情页显示
+			showOnPostPage: true,
+		},
+		{
 			// 组件类型：分类组件
 			type: "categories",
 			// 是否启用该组件
